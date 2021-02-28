cd8f3087a70c29ff082bcf45632c22bb1323f654
jessica you <jessicaxytan@gmail.com>
2021-02-27 21:56:18 -0500

    commit tree d54616dead3880287869fdc459dbf08fee882bac

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require 'pry'
 
+def my_collect(languages) #original array
+
+    count = 0 #starting point of array to run through
+    new_languages = [] #new collection of elements that will be called
+
+    while count < languages.size
+        new_languages << yield(languages[count])
+        count += 1
+    end
+
+    new_languages
+
+end
 
